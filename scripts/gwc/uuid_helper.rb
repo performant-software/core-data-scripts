edc469eb6328120da7c049c050847086c294be5f
Ben Silverman <ben@performantsoftware.com>
2025-03-03 14:07:19 -0500

    Order udf uuids by order

--- scripts/gwc/uuid_helper.rb
+++ scripts/gwc/uuid_helper.rb
@@ -3,7 +3,7 @@
 def print_uuids(project_model_id, project_model_name)
   # project_model_id should be an integer, project_model_name should be plural name string e.g. "items"
   udfs = CoreDataConnector::ProjectModel.find(project_model_id).user_defined_fields
-  udfs.each do |udf|
+  udfs.order(:order).each do |udf|
     uname = udf.column_name.parameterize.upcase.gsub '-', '_'
     uuid = udf.uuid.gsub '-', '_'
     puts "UDF_#{project_model_name.upcase}_#{uname}_UUID=\"#{uuid}\""
